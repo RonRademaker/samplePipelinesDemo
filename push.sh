ae21893b16315992beb4dfe67f13c342cce56c67
Ron Rademaker <ron@connectholland.nl>
2016-12-30 11:54:10 +0100

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 if [ "$IS_PULL_REQUEST" != true ]; then
-  sudo docker tag tutorial-ron:latest 980171072448.dkr.ecr.eu-west-1.amazonaws.com/tutorial-ron:latest
-  docker push 980171072448.dkr.ecr.eu-west-1.amazonaws.com/tutorial-ron:latest
+  sudo docker tag $IMAGE_NAME:$BRANCH.$BUILD_NUMBER 980171072448.dkr.ecr.eu-west-1.amazonaws.com/$IMAGE_NAME:$BRANCH.$BUILD_NUMBER
+  docker push 980171072448.dkr.ecr.eu-west-1.amazonaws.com/$IMAGE_NAME:$BRANCH.$BUILD_NUMBER
 else
   echo "skipping because it's a PR"
 fi
